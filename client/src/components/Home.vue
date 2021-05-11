63e3d3ef629a2712a8ecdd4ddae1b1ef7993081a
aewnix <39789678+aewnix@users.noreply.github.com>
2021-05-11 20:51:44 +0200

    Update Home.vue

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -4,7 +4,7 @@
       <Calculator></Calculator>
 
     <section id="history">
-      <h2>História výpočtov</h2>
+      <h2>História predchádzajúcich výpočtov</h2>
       <History></History>
     </section>
 
@@ -16,7 +16,8 @@
 import History from "@/components/History";
 import Calculator from "@/components/Calculator";
 import PriceSettings from "@/components/PriceSettings";
-
+//TODO: in real project I would have use the l18n for multi-language strings in app
+// TODO: use vuex to manage state between Calculator && History - rn History loads only on page load
 export default {
   name: "Home",
   components: {History,Calculator, PriceSettings},
